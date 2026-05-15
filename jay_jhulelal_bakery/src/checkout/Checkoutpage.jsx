9ccb27d09271559d68e8ba787c359e8db90f862d
mayankpritmaniprof1309 <mayankpritmaniprof@gmail.com>
2026-05-15 15:05:53 +0530

    Update Checkoutpage.jsx

--- jay_jhulelal_bakery/src/checkout/Checkoutpage.jsx
+++ jay_jhulelal_bakery/src/checkout/Checkoutpage.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useCart } from '../context/CartContext';
 import StepIndicator from './Stepindicator';
-import DeliveryForm from './deliveryform';
+import DeliveryForm from './Deliveryform';
 import PaymentForm from './Paymentform';
 import OrderReview from './OrderReview';
 import OrderSuccess from './Ordersuccess';
