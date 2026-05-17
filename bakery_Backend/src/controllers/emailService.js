3701240c8b4260b795bcb572ba5f2857902d3708
mayankpritmaniprof1309 <mayankpritmaniprof@gmail.com>
2026-05-17 14:53:38 +0530

    Fixed nodemailer IPv6 issue

--- bakery_Backend/src/controllers/emailService.js
+++ bakery_Backend/src/controllers/emailService.js
@@ -5,15 +5,26 @@ let transporter = null;
 function getTransporter() {
   if (!transporter) {
     transporter = nodemailer.createTransport({
-      host:   'smtp.gmail.com',
-      port:   587,
-      secure: false,
+      service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
+      family: 4,
+    });
+
+    transporter.verify((err, success) => {
+      if (err) {
+        console.log('SMTP ERROR:', err);
+      } else {
+        console.log('SMTP READY');
+      }
     });
   }
+
   return transporter;
 }
 
