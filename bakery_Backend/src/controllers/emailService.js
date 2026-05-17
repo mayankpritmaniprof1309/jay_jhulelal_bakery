2497dff12d31135c6c06dd65a0246af330287051
mayankpritmaniprof1309 <mayankpritmaniprof@gmail.com>
2026-05-17 14:34:21 +0530

    Update emailService.js

--- bakery_Backend/src/controllers/emailService.js
+++ bakery_Backend/src/controllers/emailService.js
@@ -2,11 +2,13 @@ const nodemailer=require('nodemailer')
 
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    }
+    host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,       // true for 465, false for 587
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
   })
 
 async function sendOrderConfirmationEmail(toEmail, userName, orderDetails) {
